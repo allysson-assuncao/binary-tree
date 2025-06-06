15e2659247b886e9959e64b228455d51a094daba
allysson-assuncao <anybody723@proton.me>
2025-06-06 19:37:00 -0300

    link

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import org.example.classes.ArvoreBinaria;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("https://code-with-me.global.jetbrains.com/QAE6eoDa2fZ48axb43ns8w#p=IU&fp=4191570DA49B864F41BADA7D415A908D972947C1AF764F2717E8D971B1537C23&newUi=true");
+        System.out.println("https://code-with-me.global.jetbrains.com/zkBEEEGBEMxDmpOjGajy6g");
 
         ArvoreBinaria arvore = new ArvoreBinaria();
         arvore.inserir(40);
